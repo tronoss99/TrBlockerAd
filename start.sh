3ce47407bcc375d8fc23486a1214bfe1c6aff9bb
pirracas 72 <nandoviverosverdejo72@gmail.com>
2026-01-04 04:04:41 +0100

    Add more blocklists and auto-run gravity on container start

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 # Start nginx in background
 nginx
 
+# Run gravity update in background after Pi-hole starts (wait 30 seconds for FTL to be ready)
+(sleep 30 && pihole -g) &
+
 # Execute the original Pi-hole entrypoint
 exec /init
